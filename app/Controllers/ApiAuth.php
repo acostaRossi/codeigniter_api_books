c79d4a0c36b9c5219578b805ef36636f83029b29
DESKTOP-JQ7HNAN\Alberto <acosta@itisrossi.vi.it>
2021-05-19 11:00:51 +0200

    Aggiunta API api/register per registrazione utente

--- app/Controllers/ApiAuth.php
+++ app/Controllers/ApiAuth.php
@@ -7,6 +7,7 @@ use \Firebase\JWT\JWT;
 use Carbon\Carbon;
 use Myth\Auth\Config\Services;
 use Config\Services as ConfigService;
+use Myth\Auth\Entities\User;
 
 class ApiAuth extends BaseController
 {
@@ -53,5 +54,68 @@ class ApiAuth extends BaseController
 		$jwt = JWT::encode($data, $this->secretKey, 'HS256');
 
 		return $this->respond($jwt, 200);
+	}
+
+	public function attemptRegister()
+	{
+		// Check if registration is allowed
+		if (! $this->config->allowRegistration)
+		{
+			return $this->failNotFound();
+		}
+
+		$users = model('UserModel');
+
+		$email = $this->request->getVar('email');
+
+		if(strpos($email, '@') === false)
+		{
+			return $this->fail('mail is not valid', 400);
+		}
+
+		// Validate here first, since some things,
+		// like the password, can only be validated properly here.
+		$rules = [
+			'username'  	=> 'required|alpha_numeric_space|min_length[3]|is_unique[users.username]',
+			'email'			=> 'required|valid_email|is_unique[users.email]',
+			'password'	 	=> 'required|strong_password',
+			'pass_confirm' 	=> 'required|matches[password]',
+		];
+
+		if (! $this->validate($rules))
+		{
+			return $this->fail(array_values(service('validation')->getErrors())[0], 400);
+		}
+
+		// Save the user
+		$allowedPostFields = array_merge(['password'], $this->config->validFields, $this->config->personalFields);
+		$user = new User($this->request->getPost($allowedPostFields));
+
+		$this->config->requireActivation !== false ? $user->generateActivateHash() : $user->activate();
+
+		// Ensure default group gets assigned if set
+        if (! empty($this->config->defaultUserGroup)) {
+            $users = $users->withGroup($this->config->defaultUserGroup);
+        }
+
+		if (! $users->save($user))
+		{
+			return $this->fail('Error during saving', 400);
+		}
+
+		if ($this->config->requireActivation !== false)
+		{
+			$activator = service('activator');
+			$sent = $activator->send($user);
+
+			if (! $sent)
+			{
+				return $this->fail('Error during sending activation email', 400);
+			}
+
+			return $this->respond('Please check your email', 200);
+		}
+
+		return $this->respond(true, 200);
 	}
 }
